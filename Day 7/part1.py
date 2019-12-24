a9689918469413e98fd45583b234034ee2271b83
Ian Harcombe <ian.harcombe@gmail.com>
2019-12-24 11:18:17 +0000

    Completed day 7!! Can't begin to say how delighted I am - but there y'go :) On to day 8 (only 16 days behind, but who's counting...)

--- Day 7/part1.py
+++ Day 7/part1.py
@@ -11,20 +11,22 @@ def run_intcode(inputs):
       line = f.readline()
       ## part 1 tests
       # (4,3,2,1,0)
-      line = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
+      #line = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
       # (0,1,2,3,4)
       # line = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+      line = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
 
       ops = list(map(int, line.split(',')))
 
       index = 0
+      output = 0
 
       full_command = ops[index]
       command = full_command % 100
       parameters = list(map(int, str(full_command // 100)))
 
       while command != 99:
-        print(command, parameters)
+        print(command, parameters, inputs, output)
         if command == 1:
             # addition
             operand1 = ops[index+1]
@@ -173,6 +175,6 @@ def part1():
   print(max(outputs))
 
 output = 0
-for phase in (4,3,2,1,0):
+for phase in (9,):
   output = run_intcode([phase, output])
 print(output)
--- Day 7/part2.py
+++ Day 7/part2.py
@@ -1,5 +1,8 @@
+import itertools
+
 class intcode:
-  def __init__(self, intfile, inputs):
+  def __init__(self, name, intfile, inputs):
+    self.name = name
     self.index = 0
     self.inputs = inputs
     self.output = 0
@@ -21,6 +24,9 @@ class intcode:
   def add_input(self, new_input):
     self.inputs.append(new_input)
 
+  def has_stalled(self):
+    return len(self.inputs) == 0 and self.command == 3
+
   def has_finished(self):
     return self.command == 99
 
@@ -38,44 +44,49 @@ class intcode:
 
   def execute_next(self):
     if not self.has_finished():
-      print(self.command, self.parameters)
+      print("{}: cmd {}, params {}, inputs {}, output {}".format(self.name, self.command, self.parameters, self.inputs, self.output))
+
       if self.command == 1:
-          # addition
-          operand1 = self.ops[self.index+1]
-          operand2 = self.ops[self.index+2]
-          dest = self.ops[self.index+3]
+        # addition
+        operand1 = self.ops[self.index+1]
+        operand2 = self.ops[self.index+2]
+        dest = self.ops[self.index+3]
 
-          mode1 = self.get_parameter()
-          mode2 = self.get_parameter()
+        mode1 = self.get_parameter()
+        mode2 = self.get_parameter()
 
-          value1 = self.ops[operand1] if mode1 == 0 else operand1
-          value2 = self.ops[operand2] if mode2 == 0 else operand2
+        value1 = self.ops[operand1] if mode1 == 0 else operand1
+        value2 = self.ops[operand2] if mode2 == 0 else operand2
 
-          result = value1 + value2
-          self.ops[dest] = result
+        print("{}: {} + {} -> {}".format(self.name, value1, value2, dest))
 
-          self.index += 4
+        result = value1 + value2
+        self.ops[dest] = result
+        self.index += 4
 
       elif self.command == 2:
-          # multiplication
-          operand1 = self.ops[self.index+1]
-          operand2 = self.ops[self.index+2]
-          dest = self.ops[self.index+3]
+        # multiplication
+        operand1 = self.ops[self.index+1]
+        operand2 = self.ops[self.index+2]
+        dest = self.ops[self.index+3]
 
-          mode1 = self.get_parameter()
-          mode2 = self.get_parameter()
+        mode1 = self.get_parameter()
+        mode2 = self.get_parameter()
 
-          value1 = self.ops[operand1] if mode1 == 0 else operand1
-          value2 = self.ops[operand2] if mode2 == 0 else operand2
+        value1 = self.ops[operand1] if mode1 == 0 else operand1
+        value2 = self.ops[operand2] if mode2 == 0 else operand2
 
-          result = value1 * value2
-          self.ops[dest] = result
+        print("{}: {} x {} -> {}".format(self.name, value1, value2, dest))
 
-          self.index += 4
+        result = value1 * value2
+        self.ops[dest] = result
+        self.index += 4
 
       elif self.command == 3:
         # input
         operand = self.ops[self.index+1]
+
+        print("{}: {} <- {}".format(self.name, operand, self.inputs))
 
         result = self.inputs.pop(0)
         self.ops[operand] = result
@@ -84,6 +95,8 @@ class intcode:
       elif self.command == 4:
         # output
         operand = self.ops[self.index+1]
+
+        print("{}: {} -> output".format(self.name, operand))
 
         self.output = self.ops[operand]
         self.index += 2
@@ -98,6 +111,8 @@ class intcode:
 
         value1 = self.ops[operand1] if mode1 == 0 else operand1
         value2 = self.ops[operand2] if mode2 == 0 else operand2
+
+        print("{}: {} if true ? {}".format(self.name, value1, value2))
 
         if value1 != 0:
           self.index = value2
@@ -115,6 +130,8 @@ class intcode:
         value1 = self.ops[operand1] if mode1 == 0 else operand1
         value2 = self.ops[operand2] if mode2 == 0 else operand2
 
+        print("{}: {} if false ? {}".format(self.name, value1, value2))
+
         if value1 == 0:
           self.index = value2
         else:
@@ -131,6 +148,8 @@ class intcode:
 
         value1 = self.ops[operand1] if mode1 == 0 else operand1
         value2 = self.ops[operand2] if mode2 == 0 else operand2
+
+        print("{}: {} < {} -> {}".format(self.name, value1, value2, operand3))
 
         if value1 < value2:
           self.ops[operand3] = 1
@@ -151,6 +170,8 @@ class intcode:
         value1 = self.ops[operand1] if mode1 == 0 else operand1
         value2 = self.ops[operand2] if mode2 == 0 else operand2
 
+        print("{}: {} == {} -> {}".format(self.name, value1, value2, operand3))
+
         if value1 == value2:
           self.ops[operand3] = 1
         else:
@@ -167,68 +188,76 @@ class intcode:
       self.parameters = list(map(int, str(full_command // 100)))
 
     else:
-      print("Running a halted machine")
+      print("{}: Running a halted machine".format(self.name))
 
   def run_to_completion(self):
     while not self.has_finished():
       self.execute_next()
     return self.get_output()
 
-def part2():
-  intfile = "test2.txt"
-  amp_a = intcode(intfile, [9, 0])
-  amp_b = intcode(intfile, [7])
-  amp_c = intcode(intfile, [8])
-  amp_d = intcode(intfile, [5])
-  amp_e = intcode(intfile, [6])
+def test_part1():
+  intfile = "test1-3.txt"
+  amp = intcode("A", intfile, [1,0])
+  output = amp.run_to_completion()
+  #print(output)
+  amp = intcode("B", intfile, [0,output])
+  output = amp.run_to_completion()
+  #print(output)
+  amp = intcode("C", intfile, [4,output])
+  output = amp.run_to_completion()
+  #print(output)
+  amp = intcode("D", intfile, [3,output])
+  output = amp.run_to_completion()
+  #print(output)
+  amp = intcode("E", intfile, [2,output])
+  output = amp.run_to_completion()
+  print(output)
+
+def run_part2(intfile, phases):
+  amp_a = intcode("A", intfile, [phases[0], 0])
+  amp_b = intcode("B", intfile, [phases[1]])
+  amp_c = intcode("C", intfile, [phases[2]])
+  amp_d = intcode("D", intfile, [phases[3]])
+  amp_e = intcode("E", intfile, [phases[4]])
 
   while not amp_e.has_finished():
-    while amp_a.has_inputs() and not amp_a.has_finished():
-      print(amp_a.get_inputs(), amp_a.get_output())
+    while not (amp_a.has_stalled() or amp_a.has_finished()):
+      #print("A", amp_a.get_inputs(), amp_a.get_output())
       amp_a.execute_next()
 
-    print("Amp A stalled with output:", amp_a.get_output())
+    print("A {} with output: {}".format("ended" if amp_a.has_finished() else "stalled", amp_a.get_output()))
     amp_b.add_input(amp_a.get_output())
-    while amp_b.has_inputs() and not amp_b.has_finished():
-      print(amp_b.get_inputs(), amp_b.get_output())
+    while not (amp_b.has_stalled() or amp_b.has_finished()):
+      #print("B", amp_b.get_inputs(), amp_b.get_output())
       amp_b.execute_next()
 
-    print("Amp B stalled with output:", amp_b.get_output())
+    print("B {} with output: {}".format("ended" if amp_b.has_finished() else "stalled", amp_b.get_output()))
     amp_c.add_input(amp_b.get_output())
-    while amp_c.has_inputs() and not amp_c.has_finished():
-      print(amp_c.get_inputs(), amp_c.get_output())
+    while not (amp_c.has_stalled() or amp_c.has_finished()):
+      #print("C", amp_c.get_inputs(), amp_c.get_output())
       amp_c.execute_next()
 
-    print("Amp C stalled with output:", amp_c.get_output())
+    print("C {} with output: {}".format("ended" if amp_c.has_finished() else "stalled", amp_c.get_output()))
     amp_d.add_input(amp_c.get_output())
-    while amp_d.has_inputs() and not amp_d.has_finished():
-      print(amp_d.get_inputs(), amp_d.get_output())
+    while not (amp_d.has_stalled() or amp_d.has_finished()):
+      #print("D", amp_d.get_inputs(), amp_d.get_output())
       amp_d.execute_next()
 
-    print("Amp D stalled with output:", amp_d.get_output())
+    print("D {} with output: {}".format("ended" if amp_d.has_finished() else "stalled", amp_d.get_output()))
     amp_e.add_input(amp_d.get_output())
-    while amp_e.has_inputs() and not amp_e.has_finished():
-      print(amp_e.get_inputs(), amp_e.get_output())
+    while not (amp_e.has_stalled() or amp_e.has_finished()):
+      #print("E", amp_e.get_inputs(), amp_e.get_output())
       amp_e.execute_next()
 
     if not amp_e.has_finished():
-      print("Amp E stalled with output:", amp_e.get_output())
+      print("E stalled with output:", amp_e.get_output())
       amp_a.add_input(amp_e.get_output())
 
-  print(amp_e.get_output())
+  print("E ended with output:", amp_e.get_output())
+  return amp_e.get_output()
 
-amp = intcode("test1-1.txt", [4,0])
-output = amp.run_to_completion()
-#print(output)
-amp = intcode("test1-1.txt", [3,output])
-output = amp.run_to_completion()
-#print(output)
-amp = intcode("test1-1.txt", [2,output])
-output = amp.run_to_completion()
-#print(output)
-amp = intcode("test1-1.txt", [1,output])
-output = amp.run_to_completion()
-#print(output)
-amp = intcode("test1-1.txt", [0,output])
-output = amp.run_to_completion()
-print(output)
+outputs = []
+for phases in itertools.permutations(range(5,10)):
+  outputs.append(run_part2("input.txt", phases))
+
+print("Max output:", max(outputs))
